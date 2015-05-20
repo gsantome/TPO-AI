bacc300f06421be665bedac0094a07ee4aa4d36e
alpinillos <apinillos@gmail.com>
2015-05-19 23:27:14 -0300

    Cambio de Nombre

---
+++ workspace/AplicacionesInteractivas/src/com/ai/models/PautaXExcesoDevolucion.java
@@ -0,0 +1,9 @@
+package com.ai.models;
+
+public class PautaXExcesoDevolucion extends Pauta {
+
+	public PautaXExcesoDevolucion() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- workspace/AplicacionesInteractivas/src/com/ai/models/XExcesoDevolucion.java
+++
@@ -1,9 +0,0 @@
-package com.ai.models;
-
-public class XExcesoDevolucion extends Pauta {
-
-	public XExcesoDevolucion() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
