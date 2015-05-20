7bcfbc6c5928dea2c5c861634e2fb10af639e5f1
alpinillos <apinillos@gmail.com>
2015-05-20 00:26:58 -0300

    ReporteColocacion

---
+++ workspace/AplicacionesInteractivas/src/com/ai/models/ReporteColocacion.java
@@ -0,0 +1,25 @@
+package com.ai.models;
+
+import java.util.ArrayList;
+
+public class ReporteColocacion {
+	private static ReporteColocacion reportador;
+	
+	public ReporteColocacion(){
+		this.getInstance();
+	}
+	
+	public static ReporteColocacion getInstance(){
+		if (reportador == null)
+		{
+			reportador = new ReporteColocacion();
+		}
+		return reportador;
+	}
+	
+	public ArrayList<ItemColocacion> getLastThreeDayItems(int IdPuesto){
+		// debe devolver la lista del mas nuevo al mas viejo. Solo los ultimos 3 dias
+		return null;
+	}
+
+}
