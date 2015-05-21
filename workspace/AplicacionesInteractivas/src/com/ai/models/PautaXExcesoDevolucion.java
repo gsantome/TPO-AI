8dfd6b7aeaee70e4f59b71bfa5d6f61899e3c5d2
alpinillos <apinillos@gmail.com>
2015-05-21 16:16:28 -0300

    PautaxExcesoDevolucion - Parte

--- workspace/AplicacionesInteractivas/src/com/ai/models/PautaXExcesoDevolucion.java
+++ workspace/AplicacionesInteractivas/src/com/ai/models/PautaXExcesoDevolucion.java
@@ -1,9 +1,38 @@
 package com.ai.models;
 
+import java.util.ArrayList;
+
+import com.ai.business.Colocacion;
+
+/*
+ * Tabla de exceso
+ *  porcentaje excedido/porcentaje de descuento
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 public class PautaXExcesoDevolucion extends Pauta {
 
 	public PautaXExcesoDevolucion() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
-
+	
+	public void procesarPauta(Colocacion colocacionActual) {
+		ArrayList<ItemColocacion> itemsColActual = colocacionActual.getItemsColocacion();
+		int cantpuestos = itemsColActual.size();
+		for (int i = 0; i < cantpuestos; i++) {
+			ItemColocacion itemColActual = itemsColActual.get(i);
+			int idpuesto = itemColActual.getCodigoPuesto();
+			ItemColocacion colocacionAnterior = ReporteColocacion.getInstance().getUltimaColocacion(idpuesto, colocacionActual.g.getIdEdicion());
+			int diferencia = colocacionAnterior.getCantidadDevoluciones()-colocacionAnterior.getCantidadEjemplares();
+			if (diferencia>0) {		
+				int descuento = PersistenciaPautas.getInstance().selectTableExceso(diferencia); //
+				itemColActual.setCantidadEjemplares(itemColActual.getCantidadEjemplares()-descuento);
+				colocacionActual.setCantEjemplares(colocacionActual.getCantEjemplares()-descuento);
+			}
+		}
+	}
 }
