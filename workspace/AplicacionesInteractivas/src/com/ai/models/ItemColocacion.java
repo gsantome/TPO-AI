f4241f0b2610089f6680d45b450a2efe2ab51419
Nicolas Morandi <nicolas.nare@gmail.com>
2015-05-21 00:04:13 -0300

    Se agregan las clases Puesto e ItemColocacion con sus atributos

--- workspace/AplicacionesInteractivas/src/com/ai/models/ItemColocacion.java
+++ workspace/AplicacionesInteractivas/src/com/ai/models/ItemColocacion.java
@@ -1,5 +1,40 @@
 package com.ai.models;
 
 public class ItemColocacion {
+	private int codigoPuesto;
+	private int cantidadEjemplares;
+	private int cantidadDevoluciones;
+	
+	public ItemColocacion() {
+		this.codigoPuesto = 0;
+		this.cantidadEjemplares = 0;
+		this.cantidadDevoluciones = 0;
+	}
+	
+	public ItemColocacion(int codigo, int ejemplares, int devoluciones) {
+		this.codigoPuesto = codigo;
+		this.cantidadEjemplares = ejemplares;
+		this.cantidadDevoluciones = devoluciones;
+	}
+
+	public int getCantidadEjemplares() {
+		return cantidadEjemplares;
+	}
+	public void setCantidadEjemplares(int cantidadEjemplares) {
+		this.cantidadEjemplares = cantidadEjemplares;
+	}
+	public int getCantidadDevoluciones() {
+		return cantidadDevoluciones;
+	}
+	public void setCantidadDevoluciones(int cantidadDevoluciones) {
+		this.cantidadDevoluciones = cantidadDevoluciones;
+	}
+	public int getCodigoPuesto() {
+		return codigoPuesto;
+	}
+	public void setCodigoPuesto(int codigoPuesto) {
+		this.codigoPuesto = codigoPuesto;
+	}
+	
 
 }
